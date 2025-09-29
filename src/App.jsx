ed0d4eee79ef52364334aa7797d58336ede99f4a
zuhaird17 <zuhaird17@gmail.com>
2025-09-29 20:00:03 +0600

    Initial commit with upgraded project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Interface from "./components/Interface"
+
+function App() {
+ 
+
+  return (
+    <>
+      <Interface/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Interface.jsx
@@ -0,0 +1,16 @@
+const Interface = () => {
+  return (
+  <div className="container mx-auto px-4 max-h-screen overflow-auto">
+  <img
+    src="/Shopping App.svg"
+    alt="Interface"
+    className="w-full h-auto max-w-[2560px]"
+    loading="lazy"
+  />
+</div>
+
+
+  );
+};
+
+export default Interface;
